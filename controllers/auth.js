e3d3f62118a0a9adcc68e6098dcb1ac55a56f66a
SerchPro <proyects_serch@hotmail.com>
2021-06-28 23:13:20 -0500

    autenticacion por google

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
 const Usuario = require('../models/usuario');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async(req, res = response) => {
@@ -45,6 +46,53 @@ const login = async(req, res = response) => {
     }
 }
 
+
+
+const googleSignin = async(req, res = response) => {
+    const { id_token } = req.body;
+
+
+    try {
+        const { correo, nombre, img } = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if (!usuario) {
+            //Sino existe crear el usuario
+            const data = {
+                nombre,
+                correo,
+                password: ':p',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrado, usuario bloqueado'
+            });
+        }
+
+        //Generar el JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        });
+
+    } catch (error) {
+
+    }
+
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignin
 }
